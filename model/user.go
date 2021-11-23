a1e39e5547aa04b376551e9593c4f77979209e70
Spencer, Paul Whiting <pwablito@gmail.com>
2021-11-23 09:00:46 -0700

    Start serialization method

--- model/user.go
+++ model/user.go
@@ -1,8 +1,17 @@
 package model
 
+import (
+	"fmt"
+)
+
 type User struct {
 	Username     string
+	Name         string
 	Email        string
 	PasswordHash string
 	PasswordSalt string
+}
+
+func (user *User) serialize() string {
+	return fmt.Sprintf("{\"username\":\"%s\",\"email\":\"%s\",\"name\":\"%s\"}", user.Username, user.Email, user.Name)
 }
