c639e308edcc176799d322a9edb86f348e496349
Spencer, Paul Whiting <pwablito@gmail.com>
2022-01-02 00:00:31 -0500

    Relationship db implementations

--- db/relationship.go
+++ db/relationship.go
@@ -17,3 +17,49 @@ func (db *Database) AddChildRelationship(child model.Child, owner string) error 
 	}
 	return nil
 }
+
+func (db *Database) AddPartnershipRelationship(partnership model.Partnership, owner string) error {
+	insertStatement := `
+		INSERT INTO partnership(id, partner1_id, partner2_id, notes, owner_username)
+		VALUES (?, ?, ?, ?, ?)
+	`
+	statement, err := db.db.Prepare(insertStatement)
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec(partnership.Id, partnership.Person1, partnership.Person2, partnership.Notes, partnership.OwnerUsername)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *Database) RemoveChildRelationship(id int) error {
+	deleteStatement := `
+		DELETE FROM child WHERE id=?
+	`
+	statement, err := db.db.Prepare(deleteStatement)
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *Database) RemovePartnershipRelationship(id int) error {
+	deleteStatement := `
+		DELETE FROM partnership WHERE id=?
+	`
+	statement, err := db.db.Prepare(deleteStatement)
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
